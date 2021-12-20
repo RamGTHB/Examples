39159276689f5910cca9cc9b5343ccc2a3a78f1c
RamGT <n1g9ab@gmail.com>
2021-12-20 23:06:40 +0300

    Ex005_IfElse

---
+++ Ex005_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введи имя: ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "саша")
+{
+    Console.WriteLine("Ура, это же САШКА!");  
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
